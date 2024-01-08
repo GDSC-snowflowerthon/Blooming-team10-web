b0b134abfbea164f09a640f965033a80e539622b
Seongna0 <sna0e@naver.com>
2024-01-08 11:11:40 +0900

    0108 NY init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+//Routing
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Manage Route in App.js
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
