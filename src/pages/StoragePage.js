dd628cdca7d854f839980fa206b28ef5ad25bdc1
23arin <127318008+23arin@users.noreply.github.com>
2024-01-12 23:19:15 +0900

    0112 AR 눈송이 보관함에 눈송이 추가

--- src/pages/StoragePage.js
+++ src/pages/StoragePage.js
@@ -1,22 +1,21 @@
 // import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import axios from "axios";
-import SnowFlower from "../components/CompletedSnowFlake";
+import SnowFlower from "../components/SFlake";
 import backIcon from '../assets/image/backIcon.svg';
-import "../styels/StoragePage.css";
+import "../styels/storagePage.css";
+// 모두 행복하기 건강하기 all A+ 받기 알고리즘 천재되기
 
 function StoragePage() {
-
   const navigate = useNavigate();
   
   return (
-    <>
     <div className="StoragePage">
         <button className="backHome" onClick={() => navigate("/")}>
             <img className="backIcon" src={backIcon} alt="backHome"/>
         </button>
         <SnowFlower />
-    </div></>
+    </div>
   );
 }
 
